0c66c31d1126af2c8fd800c30d646a57550d2ba8
dingdingwall <johndalepardillo@gmail.com>
2023-03-04 13:08:19 +0800

    new act

--- app/src/main/java/com/example/johndaleapp/MainActivity2.java
+++ app/src/main/java/com/example/johndaleapp/MainActivity2.java
@@ -2,13 +2,41 @@ package com.example.johndaleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    public static final String TAG  ="Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Log.v(TAG, "verbose nga log");
+        Log.d(TAG, "verbose nga log");
+        Log.i(TAG, "verbose nga log");
+        Log.e(TAG, "verbose nga log");
+        Log.w(TAG, "verbose nga log");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "button clicker");
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+
+
+
     }
 }
